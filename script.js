1bc961bd30fea5e3190b9a76c5665f0e65ff5d71
OlgaB <olga.bereznaja@gmail.com>
2019-10-20 23:14:03 +0100

    Вариант первый. Используется функция

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 'use strict';
 
+console.log(
+  'Вариант первый- короткий способ найдённый по ссылке с несколькими вариантами:'
+);
+
 //1- Создать переменную num, вывести в консоль произведение (умножение) цифр этого числа
 let num = 266219;
 
-num = (266219).toString().split('');
-let numTotal = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+function originNumber(number) {
+  return [...number.toString()].reduce((a, c) => a * c);
+}
+console.log(originNumber(num));
 
 //2- Полученный результат возвести в степень 3, используя только 1 оператор (не Math.pow)
-let stringToNumber = parseInt(numTotal, 10);
+let stringToNumber = parseInt(originNumber(num));
 console.log(stringToNumber);
 
 let exponent = 3;
 let result = stringToNumber ** exponent;
+console.log(result);
 
 //3- Вывести на экран первые 2 цифры полученного числа
-let finalNumber = result.toString().split('');
-
+let finalNumber = result.toString(10).split('');
+console.log(typeof finalNumber);
 console.log(finalNumber[0] + finalNumber[1]);
