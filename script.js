1668de02ccdd776e8e19f63d9869196aecbd9204
OlgaB <olga.bereznaja@gmail.com>
2019-10-20 23:08:11 +0100

    Вторая версия задания

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 'use strict';
 
 //1- Создать переменную num, вывести в консоль произведение (умножение) цифр этого числа
+
 let num = 266219;
 
-num = (266219).toString().split('');
-let numTotal = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+let array1 = num.toString().split('');
+let reducer = (accumulator, currentValue) => accumulator * currentValue;
 
-//2- Полученный результат возвести в степень 3, используя только 1 оператор (не Math.pow)
-let stringToNumber = parseInt(numTotal, 10);
-console.log(stringToNumber);
+console.log(
+  'Вариант второй- к этому решению я пришла почитав ещё раз о методе reduce() на MDN.Mozilla.'
+);
+console.log(
+  'Но мне кажется он не очень синтаксически корректен- JSHint ругается на "reducer":'
+);
+//console.log(typeof array1);
+console.log(array1.reduce(reducer));
 
+//2- Полученный результат возвести в степень 3.
+let stringToNumber = parseInt(array1.reduce(reducer));
 let exponent = 3;
 let result = stringToNumber ** exponent;
 
 //3- Вывести на экран первые 2 цифры полученного числа
 let finalNumber = result.toString().split('');
-
+//console.log(typeof finalNumber);
 console.log(finalNumber[0] + finalNumber[1]);
