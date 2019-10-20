7e3634e7d6d8289a6d95f8deb4a48e6613372ed9
OlgaB <olga.bereznaja@gmail.com>
2019-10-20 23:16:27 +0100

    Подчищены лишние console.,

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 console.log(
-  'Вариант первый- короткий способ найдённый по ссылке с несколькими вариантами:'
+  'Вариант первый- короткий способ найдённый по той ссылке с несколькими вариантами:'
 );
 
 //1- Создать переменную num, вывести в консоль произведение (умножение) цифр этого числа
@@ -14,13 +14,10 @@ console.log(originNumber(num));
 
 //2- Полученный результат возвести в степень 3, используя только 1 оператор (не Math.pow)
 let stringToNumber = parseInt(originNumber(num));
-console.log(stringToNumber);
 
 let exponent = 3;
 let result = stringToNumber ** exponent;
-console.log(result);
 
 //3- Вывести на экран первые 2 цифры полученного числа
 let finalNumber = result.toString(10).split('');
-console.log(typeof finalNumber);
 console.log(finalNumber[0] + finalNumber[1]);
